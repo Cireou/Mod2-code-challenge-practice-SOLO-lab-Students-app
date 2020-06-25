5835254ba35676291d0f7c5b3d1364b8af9ed53c
eric <eric.ou@yale.edu>
2020-06-25 12:33:06 -0700

    nice and sleak

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -6,8 +6,6 @@ class InstructorsController < ApplicationController
   end
 
   def show()
-    @sorted_students = @instructor.students.sort_by(&:name)
-    @avg = average_age()
   end
 
   private
@@ -18,11 +16,5 @@ class InstructorsController < ApplicationController
 
   def instructor_params()
     params.require(:instructor).permit(:name)
-  end
-
-  def average_age()
-    return 0 if @sorted_students.empty?
-    total_age = @sorted_students.map(&:age).sum()
-    total_age.to_f/@sorted_students.count()
   end
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,11 @@
 class Instructor < ApplicationRecord
     has_many :students
+
+    def average_age()
+        self.students.average(:age)
+    end
+
+    def sorted_students()
+        self.students.sort_by(&:name)
+    end
 end
